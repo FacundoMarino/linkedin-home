192254b3b1adeefc09a94309d75cc98b31ebaf64
FacundoMarino <facundoemarino@gmail.com>
2024-12-05 20:23:48 -0300

    feat: add util mockFeedItem

---
+++ src/utils/mockFeedItems.js
@@ -0,0 +1,20 @@
+export const mockFeedItems = [
+  {
+    id: 1,
+    avatar: "https://via.placeholder.com/50",
+    title: "John Doe",
+    subtitle: "Desarrollador Frontend",
+  },
+  {
+    id: 2,
+    avatar: "https://via.placeholder.com/50",
+    title: "Jane Smith",
+    subtitle: "Diseñadora UI/UX",
+  },
+  {
+    id: 3,
+    avatar: "https://via.placeholder.com/50",
+    title: "Mark Taylor",
+    subtitle: "Gerente de Producto",
+  },
+];
