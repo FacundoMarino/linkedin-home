af00845492250fe3d8ea84b5e806c45b913ebbf2
FacundoMarino <facundoemarino@gmail.com>
2024-12-06 12:46:40 -0300

    feat: changed react query controller

--- src/hooks/useMoviesQuery.js
+++ src/hooks/useMoviesQuery.js
@@ -1,8 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { fetchMovies } from "../api/api.service";
 
 export const useMoviesQuery = (searchTerm) => {
-  return useQuery(["movies", searchTerm], () => fetchMovies(searchTerm), {
+  return useInfiniteQuery({
+    queryKey: ["movies", searchTerm],
+    queryFn: ({ pageParam = 1 }) => fetchMovies(searchTerm, pageParam),
     enabled: !!searchTerm,
+    getNextPageParam: (lastPage, pages) => {
+      const currentPage = pages.length;
+      const totalPages = Math.ceil(parseInt(lastPage.totalResults) / 10);
+      return currentPage < totalPages ? currentPage + 1 : undefined;
+    },
   });
 };
